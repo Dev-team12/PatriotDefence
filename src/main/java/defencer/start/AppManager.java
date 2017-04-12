05bb56d4708e45a00f0940466433ace34f990ab6
james <joyukr@ukr.net>
2017-04-12 23:49:52 +0300

    # 8 fixed pupil page # 16 added new instructor page # 15 added new pupil page # 7 fixed instructor page added Patriot Defence logo

--- src/main/java/defencer/start/AppManager.java
+++ src/main/java/defencer/start/AppManager.java
@@ -13,11 +13,10 @@ public class AppManager extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        final Stage stage = new Stage();
-        Parent root = FXMLLoader.load(getClass().getResource("/pupilPage.fxml"));
-        stage.setTitle("Patriot Defence");
+        Parent root = FXMLLoader.load(getClass().getResource("/PupilPage.fxml"));
+        primaryStage.setTitle("Patriot Defence");
         Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
